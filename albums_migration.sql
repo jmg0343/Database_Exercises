c016330bf1b8053c3c227714f7c1497cddc75c94
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-16 16:20:16 -0500

    Insert exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,18 @@
 USE codeup_test_db;
+-- deletes table named albums if it exists
 DROP TABLE IF EXISTS albums;
-
+-- creates table named albums
 CREATE TABLE albums(
+	-- assigns an id that is an integer, positive number, and will automatically increment
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	artist VARCHAR(50) NOT NULL,
+	-- creates an artist column that will have a character max of 50
+	artist VARCHAR(50),
+	-- creates a column that shows the release date in 4-digit year format
 	release_date YEAR(4),
+	-- creates a column that shows the sales as a float integer
 	sales FLOAT,
+	-- creates a column that shows the genre that has a 50 character max
 	genre VARCHAR(50),
+	-- referenced id
 	PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, release_date, sales, genre)
+VALUES	("Eagles", 1976, 32.2, "Rock"),
+		("Notorious B.I.G", 1999, 32.4, "Rap"),
+		("Tupac", 1996, 34.6, "Rap");

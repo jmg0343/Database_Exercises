c33b68cb129c3f9aa6476c92c9e8079dd0312578
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-19 11:43:31 -0500

    Updated albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,8 +7,10 @@ CREATE TABLE albums(
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	-- creates an artist column that will have a character max of 50
 	artist VARCHAR(50),
+	-- creates an album name column
+	name VARCHAR(50),
 	-- creates a column that shows the release date in 4-digit year format
-	release_date YEAR(4),
+	release_date INT UNSIGNED,
 	-- creates a column that shows the sales as a float integer
 	sales FLOAT,
 	-- creates a column that shows the genre that has a 50 character max
